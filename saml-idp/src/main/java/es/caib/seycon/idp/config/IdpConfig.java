585c406cddcc4aaa33f0e1b575436c16eef0a880
Gabriel Buades <gbuades@soffid.com>
2021-05-31 17:45:12 +0200

    Separate keys for SAML and SSL encryption

--- saml-idp/src/main/java/es/caib/seycon/idp/config/IdpConfig.java
+++ saml-idp/src/main/java/es/caib/seycon/idp/config/IdpConfig.java
@@ -41,6 +41,7 @@ import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openssl.PEMEncryptor;
+import org.bouncycastle.openssl.PEMException;
 import org.bouncycastle.openssl.PEMKeyPair;
 import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.openssl.jcajce.JcaMiscPEMGenerator;
@@ -306,34 +307,16 @@ public class IdpConfig {
         
 
         // Now read the private and public key
-		PEMParser pemParser = new PEMParser(new StringReader(
-                federationMember.getPrivateKey()));
-		Object object = pemParser.readObject();
-	    JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
-
-	    keyPair = converter.getKeyPair((PEMKeyPair) object);
-		pemParser.close();
-        Key k = keyPair.getPrivate();
-        
-        JcaX509CertificateConverter converter2 = new JcaX509CertificateConverter().setProvider( "BC" );
+		Object object;
+		keyPair = readKeyPair(federationMember.getPrivateKey());
+		Key k = keyPair.getPrivate();
+		
         if (federationMember.getCertificateChain() == null) 
         {
             throw new IOException ("Missing certificate chain"); //$NON-NLS-1$
         }
         
-        certs = new LinkedList<Certificate>();
-		pemParser = new PEMParser(new StringReader(
-                federationMember.getCertificateChain()));
-		do {
-			object = pemParser.readObject();
-			if (object == null) break;
-			System.out.println(">>> object  ="+object);
-			System.out.println(">>> instance of ="+object.getClass());
-			if (object instanceof X509CertificateHolder)
-			{
-				certs.add(converter2.getCertificate((X509CertificateHolder) object));
-			}
-		} while (true);
+        certs = parseCerts(federationMember.getCertificateChain());
 
         if (federationMember.getPublicKey() == null) {
     		JcaMiscPEMGenerator gen = new JcaMiscPEMGenerator(keyPair.getPublic());
@@ -373,10 +356,46 @@ public class IdpConfig {
         String keystorePath = SeyconKeyStore.getKeyStoreFile().getPath();
         KeyStore ks = KeyStore.getInstance(SeyconKeyStore.getKeyStoreType());
         ks.load(new FileInputStream(keystorePath), p.getPassword().toCharArray());
-        ks.setKeyEntry("idp",k, p.getPassword().toCharArray(), certs.toArray(new Certificate[certs.size()])); //$NON-NLS-1$
+        if (federationMember.getSslPrivateKey() == null || federationMember.getSslPrivateKey().trim().isEmpty() ||
+        	federationMember.getSslCertificate() == null || federationMember.getSslCertificate().trim().isEmpty()) {
+        	ks.setKeyEntry("idp",k, p.getPassword().toCharArray(), certs.toArray(new Certificate[certs.size()])); //$NON-NLS-1$
+        } else {
+    		KeyPair keyPair2 = readKeyPair(federationMember.getSslPrivateKey());
+        	LinkedList<Certificate> certs2 = parseCerts(federationMember.getSslCertificate());
+        	ks.setKeyEntry("idp",keyPair2.getPrivate(), p.getPassword().toCharArray(), certs2.toArray(new Certificate[certs2.size()])); //$NON-NLS-1$
+        }
         ks.store(new FileOutputStream(keystorePath), p.getPassword().toCharArray());
         
+        
     }
+
+	protected LinkedList<Certificate> parseCerts(String pemCerts) throws IOException, CertificateException {
+		Object object;
+		JcaX509CertificateConverter converter2 = new JcaX509CertificateConverter().setProvider( "BC" );
+		LinkedList<Certificate> certs = new LinkedList<Certificate>();
+		PEMParser pemParser = new PEMParser(new StringReader(
+                pemCerts));
+		do {
+			object = pemParser.readObject();
+			if (object == null) break;
+			System.out.println(">>> object  ="+object);
+			System.out.println(">>> instance of ="+object.getClass());
+			if (object instanceof X509CertificateHolder)
+			{
+				certs.add(converter2.getCertificate((X509CertificateHolder) object));
+			}
+		} while (true);
+		return certs;
+	}
+
+	protected KeyPair readKeyPair(String pemKey) throws IOException, PEMException {
+		PEMParser pemParser = new PEMParser(new StringReader(pemKey));
+		Object object = pemParser.readObject();
+	    JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
+	    pemParser.close();
+        
+	    return converter.getKeyPair((PEMKeyPair) object);
+	}
     
     public File getConfDir () throws FileNotFoundException, IOException {
         File homeDir = Config.getConfig().getHomeDir();
