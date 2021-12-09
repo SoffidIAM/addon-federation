17fcc81bb1e42ec3a06d9ba9a020aba1fafe7e08
Gabriel Buades <gbuades@soffid.com>
2021-12-09 12:34:31 +0100

    Track server by host id rather than IP address in Kerberos authentication
    saml-idp/src/main/java/es/caib/seycon/idp/server/AuthenticationContext.java

--- saml-idp/src/main/java/es/caib/seycon/idp/ui/NtlmAction.java
+++ saml-idp/src/main/java/es/caib/seycon/idp/ui/NtlmAction.java
@@ -67,37 +67,38 @@ public class NtlmAction extends HttpServlet {
         	LogonService logonService = new RemoteServiceLocator().getLogonService();
         	ServerService serverService = new RemoteServiceLocator().getServerService();
         	
-	    	IdpConfig c = IdpConfig.getConfig();
-	        final Challenge challenge = logonService.requestChallenge(Challenge.TYPE_KERBEROS, 
-					user,
-					system,
-					c.getHostName(),
-					req.getRemoteAddr(),
-					3);
-	
-//	        challenge.setKerberosDomain(domain);
-	
-	        Sessio s = logonService.responseChallenge(challenge);  
-
-	        for (UserAccount account: serverService.getUserAccounts(challenge.getUser().getId(), c.getSystem().getName()))
-	        {
-	        	if (!account.isDisabled())
-	        	{
-            		AuthenticationContext ctx = AuthenticationContext.fromRequest(req);
-            		ctx.authenticated(account.getName(), "K", resp);
-            		ctx.store(req);
-            		if ( ctx.isFinished())
-            		{
-            			new Autenticator().autenticate2(account.getName(), getServletContext(), req, resp, ctx.getUsedMethod(), false);
-            			return;
-            		}
-	                return;
-	        	}
-	        }
     		AuthenticationContext ctx = AuthenticationContext.fromRequest(req);
-    		if (ctx != null)
-    			ctx.authenticationFailure(ctx.getUser());
-            error = String.format(Messages.getString("PasswordChangeRequiredAction.unknown.user"), principal); //$NON-NLS-1$
+    		if (ctx != null) {
+		    	IdpConfig c = IdpConfig.getConfig();
+		        final Challenge challenge = logonService.requestChallenge(Challenge.TYPE_KERBEROS,
+						user,
+						system,
+						c.getHostName(),
+						ctx.getHostId(resp),
+						3);
+		
+	//	        challenge.setKerberosDomain(domain);
+		
+		        Sessio s = logonService.responseChallenge(challenge);  
+	
+		        for (UserAccount account: serverService.getUserAccounts(challenge.getUser().getId(), c.getSystem().getName()))
+		        {
+		        	if (!account.isDisabled())
+		        	{
+	            		ctx.authenticated(account.getName(), "K", resp);
+	            		ctx.store(req);
+	            		if ( ctx.isFinished())
+	            		{
+	            			new Autenticator().autenticate2(account.getName(), getServletContext(), req, resp, ctx.getUsedMethod(), false);
+	            			return;
+	            		}
+		                return;
+		        	}
+		        }
+	    		if (ctx != null)
+	    			ctx.authenticationFailure(ctx.getUser());
+	            error = String.format(Messages.getString("PasswordChangeRequiredAction.unknown.user"), principal); //$NON-NLS-1$
+    		}
     	} catch (Exception e) {
             error = Messages.getString("UserPasswordAction.internal.error"); //$NON-NLS-1$
             LogFactory.getLog(getClass()).info("Error validating kerberos token ", e);
