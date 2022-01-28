45771d474ec2ebae1cd6e3e507a6ee644b42aeb7
Gabriel Buades <gbuades@soffid.com>
2022-01-28 22:48:55 +0100

    Apply access control rules to openid

--- saml-idp/src/main/java/es/caib/seycon/idp/shibext/DataConnector.java
+++ saml-idp/src/main/java/es/caib/seycon/idp/shibext/DataConnector.java
@@ -1,23 +1,9 @@
 package es.caib.seycon.idp.shibext;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.rmi.RemoteException;
-import java.security.InvalidKeyException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.SignatureException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
-import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
-import java.util.TimeZone;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -31,16 +17,12 @@ import com.soffid.iam.api.RoleGrant;
 import com.soffid.iam.api.User;
 import com.soffid.iam.api.UserAccount;
 import com.soffid.iam.api.UserData;
-import com.soffid.iam.api.sso.Secret;
 import com.soffid.iam.sync.engine.extobj.AccountExtensibleObject;
-import com.soffid.iam.sync.engine.extobj.AttributeReference;
-import com.soffid.iam.sync.engine.extobj.AttributeReferenceParser;
 import com.soffid.iam.sync.engine.extobj.ObjectTranslator;
 import com.soffid.iam.sync.engine.extobj.UserExtensibleObject;
 import com.soffid.iam.sync.engine.extobj.ValueObjectMapper;
 import com.soffid.iam.sync.intf.ExtensibleObject;
 import com.soffid.iam.sync.intf.ExtensibleObjectMapping;
-import com.soffid.iam.sync.service.SecretStoreService;
 import com.soffid.iam.sync.service.ServerService;
 
 import edu.internet2.middleware.shibboleth.common.attribute.BaseAttribute;
@@ -49,12 +31,9 @@ import edu.internet2.middleware.shibboleth.common.attribute.resolver.AttributeRe
 import edu.internet2.middleware.shibboleth.common.attribute.resolver.provider.ShibbolethResolutionContext;
 import edu.internet2.middleware.shibboleth.common.attribute.resolver.provider.dataConnector.BaseDataConnector;
 import edu.internet2.middleware.shibboleth.common.profile.provider.SAMLProfileRequestContext;
-import edu.internet2.middleware.shibboleth.common.session.Session;
 import es.caib.seycon.idp.client.ServerLocator;
 import es.caib.seycon.idp.config.IdpConfig;
-import es.caib.seycon.ng.exception.InternalErrorException;
 import es.caib.seycon.ng.exception.UnknownUserException;
-import es.caib.seycon.util.Base64;
 
 public class DataConnector extends BaseDataConnector {
 	Log log = LogFactory.getLog(getClass());
--- saml-idp/src/main/java/es/caib/seycon/idp/ui/CancelAction.java
+++ saml-idp/src/main/java/es/caib/seycon/idp/ui/CancelAction.java
@@ -2,35 +2,16 @@ package es.caib.seycon.idp.ui;
 
 import java.io.IOException;
 import java.net.URLEncoder;
-import java.security.Principal;
-import java.util.HashSet;
-import java.util.Set;
 
-import javax.security.auth.Subject;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.opensaml.saml2.core.AuthnContext;
-
-import com.soffid.iam.api.Password;
-
-import edu.internet2.middleware.shibboleth.idp.authn.AuthenticationEngine;
-import edu.internet2.middleware.shibboleth.idp.authn.LoginHandler;
-import edu.internet2.middleware.shibboleth.idp.authn.Saml2LoginContext;
-import edu.internet2.middleware.shibboleth.idp.util.HttpServletHelper;
-import es.caib.seycon.idp.client.PasswordManager;
-import es.caib.seycon.idp.openid.server.AuthorizationResponse;
 import es.caib.seycon.idp.openid.server.OpenIdRequest;
-import es.caib.seycon.idp.server.Autenticator;
 import es.caib.seycon.idp.server.AuthenticationContext;
 import es.caib.seycon.idp.shibext.LogRecorder;
-import es.caib.seycon.idp.shibext.SessionPrincipal;
-import es.caib.seycon.ng.exception.UnknownUserException;
 
 public class CancelAction extends HttpServlet {
     /**
