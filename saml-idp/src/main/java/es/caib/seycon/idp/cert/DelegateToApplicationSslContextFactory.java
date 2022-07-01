8ffd459ed22db32ab40c9ca11af18b71b3becec0
Gabriel Buades <gbuades@soffid.com>
2022-07-01 09:38:28 +0200

    Only ask for allowed certificates

--- saml-idp/src/main/java/es/caib/seycon/idp/cert/DelegateToApplicationSslContextFactory.java
+++ saml-idp/src/main/java/es/caib/seycon/idp/cert/DelegateToApplicationSslContextFactory.java
@@ -1,9 +1,12 @@
 package es.caib.seycon.idp.cert;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.security.KeyStore;
 import java.security.cert.CRL;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.util.Base64;
 import java.util.Collection;
 
 import javax.net.ssl.TrustManager;
@@ -19,12 +22,43 @@ import es.caib.seycon.ng.exception.InternalErrorException;
 public class DelegateToApplicationSslContextFactory extends org.eclipse.jetty.http.ssl.SslContextFactory {
 	static long lastRefresh = 0;
 	static X509Certificate[] certs = new X509Certificate[0];
+	static String dummyCertBase64 = 
+			  "MIID0TCCArmgAwIBAgIUHFuDINfO5TcDFjhqWprbd6w/hfcwDQYJKoZIhvcNAQEL\n"
+			+ "BQAweDELMAkGA1UEBhMCZXMxEzARBgNVBAgMClNvbWUtU3RhdGUxKTAnBgNVBAoM\n"
+			+ "IGF1L0thbjRsT1RRVWdiQXMrYzlVOVp6emxuV3dOVkU1MSkwJwYDVQQDDCBhdS9L\n"
+			+ "YW40bE9UUVVnYkFzK2M5VTlaenpsbld3TlZFNTAeFw0yMjA2MjMyMDM3MjNaFw0y\n"
+			+ "MjA3MjMyMDM3MjNaMHgxCzAJBgNVBAYTAmVzMRMwEQYDVQQIDApTb21lLVN0YXRl\n"
+			+ "MSkwJwYDVQQKDCBhdS9LYW40bE9UUVVnYkFzK2M5VTlaenpsbld3TlZFNTEpMCcG\n"
+			+ "A1UEAwwgYXUvS2FuNGxPVFFVZ2JBcytjOVU5Wnp6bG5Xd05WRTUwggEiMA0GCSqG\n"
+			+ "SIb3DQEBAQUAA4IBDwAwggEKAoIBAQCmtGOlKy9melEvtXnIccpH+GApCk9PSotH\n"
+			+ "ZQuGuUHSBgQsZcnQ2/Epx6qvEaWEr57T41knlhCgMxhOuya2t04cgjyDeePQT1Fo\n"
+			+ "/5KpVmt0Shp62tMxTaoyLOgDMQerqBBgIaj7Gc6ME1NClKKSBGeQKIZTvKyiQKIV\n"
+			+ "FOBYwNzomeMQceibLDfl7gRASaVYPlayKGkBAfERafRMxXn3MFWi5Hpttvt8uUAI\n"
+			+ "S+YQmQqy06/fvZAfqctr+K57a2BewdN+3KcqccuOYResuMsWiPy+2bGHHG18h7D/\n"
+			+ "mfVPGFTZqbOOOZ753RM7xX+R2sZb0GUgbld4yUt75u9GEZRtWxPhAgMBAAGjUzBR\n"
+			+ "MB0GA1UdDgQWBBTXR+nn7tznZjfOaVd7rZO4TqrVcDAfBgNVHSMEGDAWgBTXR+nn\n"
+			+ "7tznZjfOaVd7rZO4TqrVcDAPBgNVHRMBAf8EBTADAQH/MA0GCSqGSIb3DQEBCwUA\n"
+			+ "A4IBAQAtUbCt426vewxYQuv2bDXpWiZv794str+KJQRqrrrayCzqsIXU/WIHwHFY\n"
+			+ "5cR+UGF+UcdmfVwBuSSZOqK6BwNOo3emzBg9DKfddTMDiwhZEFJigVbZHwRpCGZg\n"
+			+ "+c69xhX2ULmBQX3yFMQGPaVNe/7E0YJ9EkQWBmqSzJFYh9Z8I/jNBBxGBxMgq1+u\n"
+			+ "YY0fKbMB0fI1bQE5ygzqxY1k9B+LdadiCORVM1koJ4LxLaTAXwvO95g4nrBDIpOP\n"
+			+ "cvRow78ezTIuNXaFM10J16Lg4fZ5evNyrp1+QZ2C6RBjQLqowXz553ngLBLISEYZ\n"
+			+ "+ZKUXjhoX8E1q0bFzmeRqE1Rpijw\n";
+	private static X509Certificate dummyCert = null;
 	
 	public static X509Certificate[] getTrustedCerts() {
 		if (lastRefresh < System.currentTimeMillis() - 5 * 60 * 1000) {
 			try {
 				lastRefresh = System.currentTimeMillis();
 				Collection<X509Certificate> c = new RemoteServiceLocator().getCertificateValidationService().getRootCertificateList();
+				if (c.isEmpty()) {
+					if (dummyCert == null) {
+						byte[] data = Base64.getDecoder().decode(dummyCertBase64);
+						CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+						dummyCert = (X509Certificate)certFactory.generateCertificate(new ByteArrayInputStream(data));
+					}
+					c.add(dummyCert);
+				}
 				certs = c.toArray(new X509Certificate[c.size()]);
 			} catch (Exception e) {
 				LogFactory.getLog(DelegateToApplicationSslContextFactory.class).warn("Error fetching accepted certificates", e);
