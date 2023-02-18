2b07804a3f70d91bbb5ff404c9b4c3cf42bb48b3
Gabriel Buades <gbuades@soffid.com>
2023-02-18 17:12:39 +0100

    Use smart service locator

--- saml-idp/src/main/java/com/soffid/iam/addons/federation/remote/RemoteServiceLocator.java
+++ saml-idp/src/main/java/com/soffid/iam/addons/federation/remote/RemoteServiceLocator.java
@@ -2,26 +2,37 @@ package com.soffid.iam.addons.federation.remote;
 
 import java.io.IOException;
 
+import com.soffid.iam.ServiceLocator;
 import com.soffid.iam.addons.federation.service.FederationService;
 import com.soffid.iam.addons.federation.service.UserBehaviorService;
 import com.soffid.iam.addons.federation.service.UserCredentialService;
 
+import es.caib.seycon.ng.config.Config;
 import es.caib.seycon.ng.exception.InternalErrorException;
 
 
 public class RemoteServiceLocator extends com.soffid.iam.remote.RemoteServiceLocator {
 	public FederationService getFederacioService () throws IOException, InternalErrorException 
 	{
-		return (FederationService) getRemoteService(FederationService.REMOTE_PATH);
+		if ("server".equals(Config.getConfig().getRole()))
+			return (FederationService) ServiceLocator.instance().getService(FederationService.SERVICE_NAME);
+		else
+			return (FederationService) getRemoteService(FederationService.REMOTE_PATH);
 	}
 
 	public UserBehaviorService getUserBehaviorService () throws IOException, InternalErrorException 
 	{
-		return (UserBehaviorService) getRemoteService(UserBehaviorService.REMOTE_PATH);
+		if ("server".equals(Config.getConfig().getRole()))
+			return (UserBehaviorService) ServiceLocator.instance().getService(UserBehaviorService.SERVICE_NAME);
+		else
+			return (UserBehaviorService) getRemoteService(UserBehaviorService.REMOTE_PATH);
 	}
 
 	public UserCredentialService getUserCredentialService () throws IOException, InternalErrorException 
 	{
-		return (UserCredentialService) getRemoteService(UserCredentialService.REMOTE_PATH);
+		if ("server".equals(Config.getConfig().getRole()))
+			return (UserCredentialService) ServiceLocator.instance().getService(UserCredentialService.SERVICE_NAME);
+		else
+			return (UserCredentialService) getRemoteService(UserCredentialService.REMOTE_PATH);
 	}
 }
