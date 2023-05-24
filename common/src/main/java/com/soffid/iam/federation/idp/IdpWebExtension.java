ba62ba0264f448cd747a651216a10a8ce1038f9f
Gabriel Buades <gbuades@soffid.com>
2023-05-24 18:22:41 +0200

    Remove unneeded import

--- common/src/main/java/com/soffid/iam/federation/idp/IdpWebExtension.java
+++ common/src/main/java/com/soffid/iam/federation/idp/IdpWebExtension.java
@@ -2,8 +2,6 @@ package com.soffid.iam.federation.idp;
 
 import java.util.List;
 
-import org.eclipse.jetty.servlet.ServletContextHandler;
-
 public interface IdpWebExtension {
 	List<IdpServletDescriptor> getServletDescriptors();
 }
