0346eee6b8cfa8da8a9b49a083267bbc54ddcd35
Gabriel Buades <gbuades@soffid.com>
2024-11-20 08:40:35 +0100

    Remove logs

--- saml-idp/src/main/java/es/caib/seycon/idp/ui/LogoutServlet.java
+++ saml-idp/src/main/java/es/caib/seycon/idp/ui/LogoutServlet.java
@@ -58,7 +58,6 @@ public class LogoutServlet extends HttpServlet {
     			close = true;
         	}
         	else if (isSafeLogout(req)) {
-        		log.info(">>> LOGOUT - isSafeLogout");
         		LogoutResponse r = new LogoutHandler().logout(getServletContext(), req, session, true);
         		if (r.getFrontRequests() != null && ! r.getFrontRequests().isEmpty()) {
         			JSONArray a = new JSONArray();
@@ -71,7 +70,6 @@ public class LogoutServlet extends HttpServlet {
                 	g.addArgument("logoutList", a.toString());
                 	g.addArgument("showProgress", "true");
             		new LogoutHandler().logout(getServletContext(), req, session, false);
-            		log.info(">>> LOGOUT - LogoutHandler (1)");
         		} else {
         			close = true;
         		}
@@ -82,11 +80,9 @@ public class LogoutServlet extends HttpServlet {
         		if (sessions.isEmpty()) {
             		new LogoutHandler().logout(getServletContext(), req, session, false);
         			close = true;
-        			log.info(">>> LOGOUT - LogoutHandler (2)");
         		}
         		else
         		{
-        			log.info(">>> LOGOUT - showLogout");
         			g.addArgument("showLogout", "true");
         			g.addArgument("Close_n_sessions", String.format(g.getResourceBundle().getString("Close_n_sessions"), sessions.size()));
         			StringBuffer sb = new StringBuffer();
@@ -99,25 +95,19 @@ public class LogoutServlet extends HttpServlet {
         		}
         	}
         	if (close) {
-        		log.info(">>> LOGOUT - redireccion showClose");
         		g.addArgument("showClose", "true");
         		String internal = (String) req.getSession().getAttribute("$$soffid$$-logout-internal");
         		if (!"true".equals(internal)) {
         			req.getSession().invalidate();
-        			log.info(">>> LOGOUT - Invalidar sesion");
         		} else {
-        			log.info(">>> LOGOUT - Redireccion interna, no se invalida la sesion");
         			req.getSession().removeAttribute(SessionConstants.OPENID_HOLDERGROUP);
-        			log.info(">>> LOGOUT - Se borra el holderGroup de sesion");
         		}
         		if (desiredTarget != null)
         		{
-        			log.info(">>> LOGOUT - redireccion final a "+desiredTarget);
         			resp.sendRedirect(desiredTarget);
         			return;
         		}
         	}
-        	log.info(">>> LOGOUT - Redireccion a logout.html");
        		g.generate(resp, "logout.html"); //$NON-NLS-1$
 		} catch (Exception e) {
             String error = Messages.getString("UserPasswordAction.internal.error"); //$NON-NLS-1$
@@ -125,7 +115,6 @@ public class LogoutServlet extends HttpServlet {
             LogFactory.getLog(getClass()).info("Error closing sessions ", e);
 			try {
 				g.generate(resp, "logout.html");
-				log.info(">>> LOGOUT - Redireccion por error a logout.html");
 			} catch (TextFormatException | IOException e1) {
 				throw new ServletException("Error generating logout page", e1);
 			} //$NON-NLS-1$
