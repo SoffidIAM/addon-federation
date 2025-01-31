998cd7344f9c57d4886519b7d6bb5ffa00ac17dc
Gabriel Buades <gbuades@soffid.com>
2025-01-31 11:11:44 +0000

    Update TenantFilter.java

--- saml-idp/src/main/java/es/caib/seycon/idp/ui/TenantFilter.java
+++ saml-idp/src/main/java/es/caib/seycon/idp/ui/TenantFilter.java
@@ -61,8 +61,7 @@ public class TenantFilter implements Filter {
 		res.addHeader("X-Content-Type-Options", "nosniff");
 		res.addHeader("Strict-Transport-Security", "max-age=31536000");
 		res.addHeader("Content-Security-Policy", 
-				"script-src 'self' 'unsafe-inline' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/;"
-				+ "frame-src 'self' https://www.google.com/recaptcha/ https://recaptcha.google.com/recaptcha/");
+				"script-src 'self' 'unsafe-inline' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/");
 		try {
 			IdpConfig config = IdpConfig.getConfig();
 			int realPort = request.getLocalPort();
