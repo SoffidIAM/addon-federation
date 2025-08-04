012819e9ca10ee190c51a1f774a119ae2f430059
Gabriel Buades <gbuades@soffid.com>
2025-08-04 17:19:54 +0200

    Stepped deletion of expired sessions

--- core/src/main/java/com/soffid/iam/addons/federation/model/OauthTokenEntityDaoImpl.java
+++ core/src/main/java/com/soffid/iam/addons/federation/model/OauthTokenEntityDaoImpl.java
@@ -1,6 +1,7 @@
 package com.soffid.iam.addons.federation.model;
 
 import java.util.Date;
+import java.util.List;
 
 import org.hibernate.Query;
 
@@ -24,14 +25,27 @@ public class OauthTokenEntityDaoImpl extends OauthTokenEntityDaoBase {
 
 	@Override
 	protected void handleDeleteExpiredOauthTokens() {
+		Date now = new Date();
+		do {
+			Query q = getSession().createQuery("select id "
+					+ "from com.soffid.iam.addons.federation.model.OauthTokenEntityImpl "
+					+ "where expires < :now");
+			q.setTimestamp("now", now);
+			q.setMaxResults(1000);
+			List<Long> l = q.list();
 
-		Query q = getSession().createQuery("delete from com.soffid.iam.addons.federation.model.OauthTokenScopeEntityImpl where token.id in "+
-				"(select id from com.soffid.iam.addons.federation.model.OauthTokenEntityImpl where expires < :now)");
-		q.setTimestamp("now", new Date());
-		q.executeUpdate();
-
-		q = getSession().createQuery("delete from com.soffid.iam.addons.federation.model.OauthTokenEntityImpl where expires < :now");
-		q.setTimestamp("now", new Date());
-		q.executeUpdate();
+			if (l.isEmpty())
+				 return;
+			
+			q = getSession().createQuery("delete from com.soffid.iam.addons.federation.model.OauthTokenScopeEntityImpl "
+					+ "where token.id in :l");
+			q.setParameterList("l", l);
+			q.executeUpdate();
+			
+			q = getSession().createQuery("delete from com.soffid.iam.addons.federation.model.OauthTokenEntityImpl "
+					+ "where id in :l");
+			q.setParameterList("l", l);
+			q.executeUpdate();
+		} while (System.currentTimeMillis() - now.getTime() < 60_000); // 1 minute maximum
 	}
 }
